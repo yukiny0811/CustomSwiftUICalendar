aa1de3d5d77cde6257e96cfdc55c00ef6003c578
yukiny0811 <yukiny0811@gmail.com>
2024-06-08 20:02:33 +0900

    initial commit

---
+++ CustomSwiftUICalendarExample/CustomSwiftUICalendarExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomSwiftUICalendarExample
+//
+//  Created by Yuki Kuwashima on 2024/06/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomSwiftUICalendarExample/CustomSwiftUICalendarExample/CustomSwiftUICalendarExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSwiftUICalendarExampleApp.swift
+//  CustomSwiftUICalendarExample
+//
+//  Created by Yuki Kuwashima on 2024/06/08.
+//
+
+import SwiftUI
+
+@main
+struct CustomSwiftUICalendarExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
